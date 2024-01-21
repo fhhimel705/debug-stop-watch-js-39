dce1a24382435b1a03c16fae1a49241256dff61a
FH Himel <fhhimel17@gmail.com>
2024-01-22 03:19:04 +0600

    third commit

--- stop-watch.js
+++ stop-watch.js
@@ -1,17 +1,25 @@
 let inter;
+let num = 0;
+const time = document.getElementById("time");
+
 const setTime = () => {
-  const time = document.getElementById("time");
-  let num = 0;
   inter = setInterval(() => {
-    time.innerText = `${num++}`;
+    time.innerText = num++;
   }, 1000);
   return inter;
 };
 document.getElementById("start-btn").addEventListener("click", function () {
     setTime();
+    
 });
 
 document.getElementById("stop-btn").addEventListener("click", function () {
     clearInterval(inter);
+});
+
+document.getElementById("reset-btn").addEventListener("click", function () {
+    clearInterval(inter);
+    num = 0;
+    time.innerText = 0;
 });
 
